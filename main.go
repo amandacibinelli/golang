53b1e10294cccb3f7a1d9734345b7c6773042820
amanda <amanda.cibinelli@aluno.senai.br>
2025-03-28 08:04:25 -0300

    artméticos

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"  
+import "fmt"
 
-func main(){
-var x int = 10
-x += 5
-fmt.Printf("O valor de x eh" , x)
+func main() {
+	
+	a , b := 10 , 3
+
+	fmt.Println("A soma eh" , a + b)
+	fmt.Println("A subtraçao: eh" , a - b)
+	fmt.Println("A divisao eh" , a / b)
+	fmt.Println("A multiplicaçao eh" , a * b)
+	fmt.Println("O resto da divisao eh" , a % b)
 
 }
