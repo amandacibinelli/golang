f91db23fd95ffa38c64b9f155adef80171530b7e
amanda <amanda.cibinelli@aluno.senai.br>
2025-04-03 09:20:42 -0300

    slice

--- main.go
+++ main.go
@@ -9,5 +9,21 @@ fmt.Println(ages)
 fmt.Println(nomes)
 nomes[3] = "Roberto"
 fmt.Println(ages)
+// Slice
+var score =[]int{100, 200, 300, 400}
+fmt.Println(score)
+score[1] = 2
+fmt.Println(score, len(score), cap(score))
+rangeOne := score[1:3]
+fmt.Println(rangeOne)
+rangeTwo := score[2:]
+fmt.Println(rangeTwo)
+rangeThree := score[3]
+fmt.Println(rangeThree)
 
-}
+var amigos = []string{"Ana", "Julia", "Lorena"}
+fmt.Println(amigos)
+amigos = append(amigos, "isa", "yas", "manu", "heitor")
+fmt.Println(amigos, len(amigos), cap(amigos))
+
+}                
