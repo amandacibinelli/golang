94ee6d5062687c7f1d8e7418b1189ab9b03b8bdb
amanda <amanda.cibinelli@aluno.senai.br>
2025-04-03 08:22:46 -0300

    vetores

--- main.go
+++ main.go
@@ -2,21 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
-var usuario string
-var senha int
-fmt.Println ("Digte seu usuario")
-fmt.Scan(&usuario)
-if usuario == "admin" {
-fmt.Println("Digite sua senha")
-fmt.Scan(&senha) 
-if senha == 1234 {
-fmt.Println("acesso permitido")
-} else {
-fmt.Println("senha incorreta")
-}
-} else { 
-fmt.Println("usuario incorreto")
-}
+func main(){
+var ages = [4]int{17, 16, 20, 40}
+nomes := [4]string{"Amanda", "Ana", "Julia", "Lorena"}
+fmt.Println(ages)
+fmt.Println(nomes)
+nomes[3] = "Roberto"
+fmt.Println(ages)
 
 }
